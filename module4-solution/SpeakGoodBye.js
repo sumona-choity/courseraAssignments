9ea65eb98e3c1866262faa0088f7f5aec44efbc2
sumona-choity <choity.sumona@gmail.com>
2021-08-01 00:54:40 -0400

    module 4 solution

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -18,7 +18,7 @@
 // See Lecture 52, part 2
 // function speak(name) {
 //   console.log(speakWord + " " + name);
-}
+// }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
