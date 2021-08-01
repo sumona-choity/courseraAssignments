924a0533ca09a432b66756626f5bffdf98afb6a0
sumona-choity <choity.sumona@gmail.com>
2021-08-01 01:19:27 -0400

    module 4 solution

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -46,7 +46,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (var i = 0; i < names.length; i++) {
+for (var i = 0; i <= names.length; i++) {
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
