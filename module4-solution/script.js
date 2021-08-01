8c9ef071178596a33365ebbc46414076adfbaf6a
sumona-choity <choity.sumona@gmail.com>
2021-08-01 00:53:15 -0400

    module 4 solution

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,6 +33,11 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
+
+helloSpeaker.speak();
+byeSpeaker.speak();
+
+
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
